7272f38454d1551e911239b63756f1c81ef34bad
Ubuntu <ubuntu@nare.com>
2019-07-19 10:50:57 +0000

    news edit 2

--- new2.sh
+++ new2.sh
@@ -5,3 +5,15 @@ the square value of the number, 5.
 '
 ((area=5*5))
 echo $area
+
+
+echo "Enter username"
+read username
+echo "Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "1234" ) ]]; then
+echo "valid user"
+else
+echo "invalid user"
+fi

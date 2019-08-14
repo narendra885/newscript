8bea61d70959fa3a0244b788d5e0c97708545903
narendra885 <52396212+narendra885@users.noreply.github.com>
2019-08-14 10:33:25 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -8,16 +8,5 @@ read answer
 fine
 echo "me too $answer!"
 
-<<<<<<< HEAD
-for jenkins 
-=======
 
-this update for git pull
-malli check
->>>>>>> fb99840c41c7f63eda13b001866ed388bad30c44
-
-new
-new2
-hi
-git push trigger
 
